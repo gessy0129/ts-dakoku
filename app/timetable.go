c8a7551684fb531755b7d0b1b665932400faa6f2
下司宜治 <geshi@gesuyochinoMacBook-Pro.local>
2020-01-14 21:54:37 +0900

    debug文追加

--- app/timetable.go
+++ app/timetable.go
@@ -155,6 +155,7 @@ func (ctx *Context) createTimeTableClient() *timeTableClient {
 
 func (client *timeTableClient) doRequest(method string, data io.Reader) ([]byte, error) {
 	req, err := http.NewRequest(method, client.Endpoint, data)
+	fmt.Printf("%v %v %v\n", method, client.Endpoint, data)
 	if err != nil {
 		return nil, err
 	}
